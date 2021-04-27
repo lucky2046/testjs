3b30c3d0e330c472cf6167e4a8075be977c48e8f
luckytest <11626708+luckywuhan@users.noreply.github.com>
2021-04-27 09:54:42 +0800

    Update test.js

--- test.js
+++ test.js
@@ -49,6 +49,7 @@ if ($.isNode()) {
   for (let i = 0; i < cookiesArr.length; i++) {
     if (cookiesArr[i]) {
       cookie = cookiesArr[i];
+      console.log(cookie);
       $.UserName = decodeURIComponent(cookie.match(/pt_pin=([^; ]+)(?=;?)/) && cookie.match(/pt_pin=([^; ]+)(?=;?)/)[1])
       $.index = i + 1;
       $.beanCount = 0;
